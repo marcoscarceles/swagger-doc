90e56fb627cf65e5ac2b55a52a030b1d28baf0cb
marcos-carceles <marcos.carceles@gmail.com>
2015-04-21 15:08:36 +0200

    Moved marshaller tests to unit specs

--- test/unit/com/makroos/grails/plugins/swaggerdoc/marshaller/MarshallersHelperSpec.groovy
+++ test/unit/com/makroos/grails/plugins/swaggerdoc/marshaller/MarshallersHelperSpec.groovy
@@ -1,6 +1,7 @@
 package com.makroos.grails.plugins.swaggerdoc.marshaller
 
 import com.wordnik.swagger.models.Info
+import com.wordnik.swagger.models.Path
 import com.wordnik.swagger.models.Swagger
 import com.wordnik.swagger.models.Tag
 import grails.converters.JSON
@@ -9,23 +10,24 @@ import spock.lang.Specification
 /**
  * Created by @marcos-carceles on 16/04/15.
  */
-class MarshallersSpec extends Specification {
+class MarshallersHelperSpec extends Specification {
 
     void "marshalled objects must not contain class data"() {
         when:
-        List<JSON> jsonObjects = [Swagger, Info, Tag].collect { Class it ->
-            it.newInstance() as JSON
+        List<Map> jsonObjects = [Swagger, Info, Tag, Path].collect { Class it ->
+            MarshallerHelper.MARSHALLER_WRAPPER(it.newInstance())
         }
 
         then:
         jsonObjects.every {
+            !it.containsKey('class')
             !(it as String =~ /"?class"?:/)
         }
     }
 
     void "info JSON does not include vendorExtensions"() {
         when:
-        JSON infoJson = new Info() as JSON
+        Map infoJson = MarshallerHelper.MARSHALLER_WRAPPER(new Info())
         then:
         !(infoJson as String =~ /"?vendorExtensions"?:/)
     }
