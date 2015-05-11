a64b7d0972091cdc080cd46c6846a5fb82122b99
marcos-carceles <marcos.carceles@gmail.com>
2015-05-11 13:58:21 -0500

    Version Bump

--- SwaggerDocGrailsPlugin.groovy
+++ SwaggerDocGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.codehaus.groovy.grails.commons.GrailsApplication
 
 class SwaggerDocGrailsPlugin {
     // the plugin version
-    def version = "0.1-SNAPSHOT"
+    def version = "0.1-RC2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
